a0a06f9a45aa2324cad98551c0eeab447870e6b6
Subhamk2004 <subhamk1978@gmail.com>
2024-12-23 22:18:46 +0530

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        softGray: '#F2F2F2',
+        darkGray: '#D6D6D6',
+        primary: '#050508',
+        secondary: '#111525',
+        btnclr:'#4151d8',
+        textp: '#494949',
+        alertclr:'#df5252',
+        purple:'#4d176a',
+        lime:'#b2ea37'
+      },
+    },
+  },
+  plugins: [],
+}
+
